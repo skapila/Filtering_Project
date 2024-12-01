5708a6ce894605c67210c85b52339e354be9061a
unknown <samkapila95@gmail.com>
2024-12-01 18:33:18 +0530

    Setup sensor simultion, state transition matrix, Process Noise matrix in 2D_Tracking_Robot

---
+++ 2D_Tracking_Robot/SensorSimulation.py
@@ -0,0 +1,30 @@
+from numpy.random import randn
+
+class PosSensor(object):
+    def __init__(self, pos=(0, 0), vel=(0, 0), noise_std=1.):
+        self.vel = vel
+        self.noise_std = noise_std
+        self.pos = [pos[0], pos[1]]
+        
+    def read(self):
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+        
+        return [self.pos[0] + randn() * self.noise_std,
+                self.pos[1] + randn() * self.noise_std]
+    
+
+
+class PosSensor_1(object):
+    def __init__(self, pos=(0, 0), vel=(0, 0), noise_std=1., time_step=1.):
+        self.vel = vel
+        self.noise_std = noise_std
+        self.pos = [pos[0], pos[1]]
+        self.time_step=time_step
+        
+    def read(self):
+        self.pos[0] += (self.vel[0]*self.time_step)
+        self.pos[1] += (self.vel[1]*self.time_step)
+        
+        return [self.pos[0] + randn() * self.noise_std,
+                self.pos[1] + randn() * self.noise_std]    
