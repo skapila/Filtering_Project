64a31cf7bfdf448e8de0f9a2cb147deb36a3c045
Samarth Kapila <samkapila95@gmail.com>
2024-12-06 16:52:06 +0530

    Design second order  kalman filter

--- 2DTrackingRobot/ConstantAcceleration/run.py
+++ 2DTrackingRobot/ConstantAcceleration/run.py
@@ -2,28 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from filterpy.stats import plot_covariance_ellipse
 
-from kalman_filter_design import Tracker2DConstantVelocity as Tracker2D
+from kalman_filter_design import Tracker2DConstantAcceleration as Tracker2D
 from sensor_simulation import PosSensor,PosSensor1
 from ploting import *
 
 # Setting Q and R 
 Q_std=0.01
-R_std=0.35
+R_std=2
 dt=1.0
 
 # simulate robot movement
 N = 30
-sensor = PosSensor((0, 0), (2, .2), noise_std=R_std)
+sensor = PosSensor((0, 0), (0, 0), (0.1,0.2), noise_std=R_std)
 zs = np.array([sensor.read() for _ in range(N)])
 
 # run filter
-robot_tracker = Tracker2D(Q_std,R_std,dt).kf_2D_CV
+robot_tracker = Tracker2D(Q_std,R_std,dt).kf_2D_CA
 mu, cov, _, _ = robot_tracker.batch_filter(zs)
 
 # output result
-plot_measurement_estimate(mu,0,2,zs,0,1)
-plot_2d_ellipse(mu,cov,0,2)
-plot_2d_ellipse_measurement_estimate(mu,cov,0,2,zs,0,1)
+plot_measurement_estimate(mu,0,3,zs,0,1)
+plot_2d_ellipse(mu,cov,0,3)
+plot_2d_ellipse_measurement_estimate(mu,cov,0,3,zs,0,1)
 plot_residue_variance(mu,cov,0,zs,0,dt)
 plot_mean_variance(mu,cov,0,dt)
 plot_mean_variance_measurement(mu,cov,0,zs,0,dt)
---
+++ 2DTrackingRobot/ConstantAcceleration/sensor_simulation.py
@@ -0,0 +1,40 @@
+from numpy.random import randn
+
+class PosSensor(object):
+    def __init__(self, pos=(0, 0),vel=(0, 0), accel=(0, 0), noise_std=1.):
+        self.noise_std = noise_std
+        self.pos = [pos[0], pos[1]]
+        self.vel = [vel[0], vel[1]]
+        self.accel = [accel[0],accel[1]]
+
+    def read(self):
+        self.vel[0] += self.accel[0]
+        self.vel[1] += self.accel[1]
+
+        self.pos[0] += self.vel[0]
+        self.pos[1] += self.vel[1]
+        
+        return [self.pos[0] + randn() * self.noise_std,
+                self.pos[1] + randn() * self.noise_std]
+    
+
+
+class PosSensor1(object):
+    def __init__(self, pos=(0, 0), vel=(0, 0), accel=(0, 0), noise_std=1., time_step=1.):
+        self.vel = vel
+        self.noise_std = noise_std
+    
+        self.pos = [pos[0], pos[1]]
+        self.vel = [vel[0], vel[1]]
+        self.accel = [accel[0],accel[1]]
+        self.time_step=time_step
+        
+    def read(self):
+        self.vel[0] += (self.accel[0]*self.time_step)
+        self.vel[1] += (self.accel[1]*self.time_step)
+
+        self.pos[0] += (self.vel[0]*self.time_step)
+        self.pos[1] += (self.vel[1]*self.time_step)
+        
+        return [self.pos[0] + randn() * self.noise_std,
+                self.pos[1] + randn() * self.noise_std]    
